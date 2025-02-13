fadba5127f6e695648d79bc8ce52c94cdebba83c
DmitriiPl <Plighig@mail.ru>
2025-02-13 18:09:05 +0300

    testing neogit

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,5 +1,16 @@
 return { 
     "nvim-neo-tree/neo-tree.nvim", 
     branch = "v3.x", 
-    dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", "MunifTanjim/nui.nvim",} 
+    dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", "MunifTanjim/nui.nvim",},
+    config = function()
+        require("neo-tree").setup({
+          filesystem = {
+            filtered_items = {
+              visible = true,  -- Показывать все файлы (не скрывать по умолчанию)
+              hide_dotfiles = false,  -- Не скрывать dot-файлы
+              hide_gitignored = false, -- Не скрывать игнорируемые git файлы
+            },
+          },
+        })
+    end
 } 
